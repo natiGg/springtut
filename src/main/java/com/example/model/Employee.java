36e0bc861748bafac23a2974b0a08fd61398860c
natiGg <nathangere12@gmail.com>
2023-04-09 11:21:29 -0700

    initial

---
+++ src/main/java/com/example/model/Employee.java
@@ -0,0 +1,54 @@
+package com.example.model;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@EnableAutoConfiguration
+@Entity
+@Table(name="employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    public Employee() {
+    }
+    public Employee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+
+}
